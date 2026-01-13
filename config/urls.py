a3bf5028c96a3c570e58569a5ea98dfe9cfe2fd4
Fabio Hams Valois <92942417+Neon18H@users.noreply.github.com>
2026-01-12 21:31:35 -0500

    Add bootstrap login form

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import include, path
+
+from finance.forms import BootstrapAuthenticationForm
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("finance.urls")),
+    path(
+        "accounts/login/",
+        LoginView.as_view(authentication_form=BootstrapAuthenticationForm),
+        name="login",
+    ),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
